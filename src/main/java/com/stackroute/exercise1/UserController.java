b77f462769c89498e24e51d6328760b21ae4a2da
priyanka <priyanka150skct@gmail.com>
2019-07-18 11:19:22 +0530

    completed SpringMVC exercise1

---
+++ src/main/java/com/stackroute/exercise1/UserController.java
@@ -0,0 +1,20 @@
+package com.stackroute.exercise1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController
+{
+    @RequestMapping("/")
+    public String display(Model model)
+    {
+        User user=new User();
+        user.setName("priya");
+        String name=user.getName();
+        model.addAttribute("print",name);
+        return "index";
+    }
+
+}
